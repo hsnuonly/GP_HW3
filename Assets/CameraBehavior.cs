a831739348e9056b4589043dbf2e7938df65d739
hsnuonly <hsnuonly@gmail.com>
2018-11-11 18:35:27 +0800

    init

---
+++ Assets/CameraBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public Transform player;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = this.transform.position;
+		temp.x = player.position.x;
+		this.transform.position = temp;
+    }
+}
